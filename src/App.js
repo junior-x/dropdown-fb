98bed084f09d4073a5b4fea53460bd52a05f9202
junior-x <marcosmansur@outlook.com>
2022-01-30 12:21:29 -0800

    ufa

---
+++ src/App.js
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <Navbar />
+  );
+}
+
+function Navbar() {
+  return( 
+    <nav className='navbar'>
+      <ul className='navbar-nav'></ul>
+    </nav>
+  )
+}
