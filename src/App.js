41fb32d24900004a48aa02c549f889972fd14149
junior-x <marcosmansur@outlook.com>
2022-01-30 13:11:48 -0800

    buttons&&navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
+import { ReactComponent as BellIcon } from "./icons/bell.svg";
+import { ReactComponent as MessangerIcon } from "./icons/messenger.svg";
+import { ReactComponent as CaretIcon } from "./icons/caret.svg";
+import { ReactComponent as PlusIcon } from "./icons/plus.svg";
+import { ReactComponent as CogIcon } from "./icons/cog.svg";
+import { ReactComponent as ChevronIcon } from "./icons/chevron.svg";
+import { ReactComponent as ArroIcon } from "./icons/arrow.svg";
+import { ReactComponent as BoltIcon } from "./icons/bolt.svg";
+
+import React, { useState } from "react";
+
 export default function App() {
   return (
-    <Navbar />
+    <Navbar>
+      <NavItem icon={ <PlusIcon/> } />
+      <NavItem icon={ <BellIcon/> } />
+      <NavItem icon={ <MessangerIcon/> } />
+
+      <NavItem icon={ <CaretIcon/> }>
+
+      </NavItem>
+    </Navbar>
   );
 }
 
-function Navbar() {
+function Navbar(props) {
   return( 
     <nav className='navbar'>
-      <ul className='navbar-nav'></ul>
+      <ul className='navbar-nav'> { props.children }</ul>
     </nav>
+  )
+}
+
+function NavItem(props) {
+
+  const [open, setOpen] = useState();
+
+  return (
+    <li className="nav-item" >
+      <a href="#" className="icon-button" >
+        {props.icon}
+      </a>
+    </li>
   )
 }
